454537caa321274d783121dc6c6142c97d72c25f
budak <thatbudakguy@gmail.com>
2016-03-22 10:06:43 -0700

    add bio_base
    - adds bio_base.json packer template
    - adds shell script to mount external drive

--- add_greta_data.sh
+++ add_greta_data.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # download greta's data and build a new blast database from it
 sleep 30
-sudo su
-cd /blast/blastdb_custom/
+mkdir /data/greta_data
+cd /data/greta_data
 wget http://files.cgrb.oregonstate.edu/CGRB/Kronmiller/downloads/binford20151205/redo_protein_blasts/all_redo_pep.fasta
 makeblastdb -in all_redo_pep.fasta -dbtype 'prot' -out greta_db
-sleep 30
