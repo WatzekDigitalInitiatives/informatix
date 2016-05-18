c3d101bf88edadc079e4f1ab1f1ecac489b216ef
budak <thatbudakguy@gmail.com>
2016-05-18 16:29:52 -0700

    update old db scripts
    [ci skip]

--- shell/add_greta_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# download greta's data and build a new blast database from it
-mkdir /data/greta_data
-cd /data/greta_data
-wget http://library.lclark.edu/testpages/fasta/all_redo_pep.fasta
-makeblastdb -in all_redo_pep.fasta -dbtype 'prot' -out greta_db
--- shell/add_greta_data_summer16.sh
+++ shell/add_greta_data_summer16.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # download greta's data and build a new blast database from it
 cd /blast/blastdb_custom/
-wget http://library.lclark.edu/testpages/fasta/ComboDatabase2.fasta
+wget #link to db file
 makeblastdb -in ComboDatabase2.fasta -dbtype 'prot' -out illumina_cDNA_smd
