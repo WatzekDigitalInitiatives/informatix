719b5a819929f53cfd5ccb0247f1c38674600ce3
budak <thatbudakguy@gmail.com>
2016-05-18 10:48:44 -0700

    add summer '16 script

--- add_greta_data.sh
+++ add_greta_data.sh
@@ -2,5 +2,5 @@
 # download greta's data and build a new blast database from it
 mkdir /data/greta_data
 cd /data/greta_data
-wget http://files.cgrb.oregonstate.edu/CGRB/Kronmiller/downloads/binford20151205/redo_protein_blasts/all_redo_pep.fasta
+wget http://library.lclark.edu/testpages/fasta/all_redo_pep.fasta
 makeblastdb -in all_redo_pep.fasta -dbtype 'prot' -out greta_db
