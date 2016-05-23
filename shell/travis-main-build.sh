925a87dcc5a7b883832f10c4f7e31d9eed7fea65
budak <thatbudakguy@gmail.com>
2016-05-23 14:31:06 -0700

    shallow clone only *.py files
    [ci skip]

--- shell/travis-main-build.sh
+++ shell/travis-main-build.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 set -ev # break script on nonzero exit code, ending build. also return verbose bash output.
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+python --version # or run some tests, etc.
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then # only build an AMI if we're a commit, not a PR
 	packer build bio_base.json
 fi
