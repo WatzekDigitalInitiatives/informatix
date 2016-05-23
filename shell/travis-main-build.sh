d17022080a1f103084b9c74c71a15ebbd9eef506
budak <thatbudakguy@gmail.com>
2016-05-23 13:38:20 -0700

    clean up main build script
    [ci skip]

--- shell/travis-main-build.sh
+++ shell/travis-main-build.sh
@@ -1,8 +1,5 @@
 # !/bin/bash
 set -ev # break script on nonzero exit code, ending build. also return verbose bash output.
-# run tests on our python scripts
-
-# if we're not a pull request check, also build the AMI
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	packer build bio_base.json
 fi
