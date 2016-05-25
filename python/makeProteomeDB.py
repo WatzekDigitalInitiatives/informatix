0f8ce8067f1bb3cc9b94cbfb4c7297605bd5f9d8
Rishi <rishijavia@lclark.edu>
2016-05-25 09:08:47 -0700

    added loop functionality to makeProteomeDB
    [ci skip]

--- python/makeProteomeDB.py
+++ python/makeProteomeDB.py
@@ -7,24 +7,27 @@ import sys
 import bioio
 import biomath
 
-# strip file extension and read file
-input_txt_name = sys.argv[-1][:-4]
-input_txt_data = bioio.readTXT(sys.argv[-1])
+files = sys.argv[1:]
 
-# add greaterthans
-output_data = bioio.addGreaterThans(input_txt_data)
+for file in files:
+	# strip file extension and read file
+	input_txt_name = file[:-4]
+	input_txt_data = bioio.readTXT(sys.argv[-1])
 
-# split on greaterthans
-output_data = bioio.splitLinearSeqids(output_data)
+	# add greaterthans
+	output_data = bioio.addGreaterThans(input_txt_data)
 
-# add venom codes based on filename
-output_data = bioio.addVenomCode(output_data,input_txt_name)
+	# split on greaterthans
+	output_data = bioio.splitLinearSeqids(output_data)
 
-# replace s??? codes with sample info code
-output_data = bioio.replaceSCodes(output_data)
+	# add venom codes based on filename
+	output_data = bioio.addVenomCode(output_data,input_txt_name)
 
-# define names of the resulting file
-output_txt_name = input_txt_name+"_proteome_ready.txt"
+	# replace s??? codes with sample info code
+	output_data = bioio.replaceSCodes(output_data)
 
-# write the resulting file
-bioio.writeTXT(output_txt_name,output_data)
+	# define names of the resulting file
+	output_txt_name = input_txt_name+"_proteome_ready.txt"
+
+	# write the resulting file
+	bioio.writeTXT(output_txt_name,output_data)
