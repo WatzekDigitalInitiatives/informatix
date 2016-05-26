958d809cd9bd46c56d5a0f683e7a836d50571677
budak <thatbudakguy@gmail.com>
2016-05-26 14:32:25 -0700

    go to test/ before testing!
    derp

--- shell/travis-main-build.sh
+++ shell/travis-main-build.sh
@@ -1,11 +1,8 @@
 # !/bin/bash
 set -ev # break script on nonzero exit code, ending build. also return verbose bash output.
-python --version
-python python/test/unittests.py
-ls -al
-ls -al python/
-ls -al python/test/
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] # only build an AMI if we're a commit, not a PR
 	then
 		packer build bio_base.json
+	else
+		cd python/test &&	python unittests.py
 fi
