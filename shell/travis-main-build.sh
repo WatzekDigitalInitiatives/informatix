63c7b5c37fb4cd4f162f32f49509d13b3bf72952
budak <thatbudakguy@gmail.com>
2016-05-26 14:05:26 -0700

    use ls to check what travis is cloning

--- shell/travis-main-build.sh
+++ shell/travis-main-build.sh
@@ -2,7 +2,10 @@
 set -ev # break script on nonzero exit code, ending build. also return verbose bash output.
 python --version
 python python/test/unittests.py
+ls -al
+ls -al python/
+ls -al python/test
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] # only build an AMI if we're a commit, not a PR
 	then
-		packer build bio_base.json  
+		packer build bio_base.json
 fi
