c3d6754a5d34b5b653625e469e43ab99f9f396b2
budak <thatbudakguy@gmail.com>
2016-10-28 14:59:18 -0700

    adjust dependency install stage

--- shell/travis-install-dependencies.sh
+++ shell/travis-install-dependencies.sh
@@ -1,14 +1,10 @@
 # !/bin/bash
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]
 	then
-    # update package manager
-    sudo apt-get update
     # install unzip and wget if we don't have them
     sudo apt-get install -y unzip wget
     # install packer on travis machine to build ami
-    cd ~
-    wget https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
-    chmod 777 packer_0.10.1_linux_amd64.zip
+    wget https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip && chmod +x packer_0.10.1_linux_amd64.zip
     unzip packer_0.10.1_linux_amd64.zip
     sudo cp packer /usr/bin/
 fi
